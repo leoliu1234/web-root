f9496dc54ca5a511d3c528843eef9b8c82ce225a
T04395 <Leo.Liu@augmentum.com.cn>
2019-03-29 12:27:34 +0800

    refine code

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,26 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <img alt="Vue logo" src="../assets/logo.png" @click="showMessage()"> {{message}}
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
+import { Component, Vue, Provide } from 'vue-property-decorator';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 @Component({
   components: {
     HelloWorld,
-  },
+  }
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  // data
+  private message: string = 'message'
+
+  // methods
+  private showMessage(): void {
+    alert(this.message)
+  }
+}
 </script>
